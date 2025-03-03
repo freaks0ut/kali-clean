b515a1cb2fc390e82adb126c34714fd9afc1cec1
Manuel Cerny <mc245.root@gmail.com>
2025-03-03 23:40:25 +0100

    updated for arm64

--- install.sh
+++ install.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
+# Update and upgrade system
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install alacritty -y
-sudo apt install -y wget curl git thunar
-sudo apt install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
-sudo apt install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
-sudo apt install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
+# Install essential packages
+sudo apt install -y alacritty wget curl git thunar \
+    arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance \
+    python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick \
+    libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
+    libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev \
+    libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev \
+    libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev \
+    autoconf meson ninja-build
 
+# Install additional XCB dependencies for i3-gaps
+sudo apt install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev
+
+# Download and install Nerd Fonts
 mkdir -p ~/.local/share/fonts/
-
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
-
 unzip Iosevka.zip -d ~/.local/share/fonts/
 unzip RobotoMono.zip -d ~/.local/share/fonts/
-
 fc-cache -fv
 
-wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.0-rc4-1/alacritty_0.10.0-rc4-1_amd64_bullseye.deb
-sudo dpkg -i alacritty_0.10.0-rc4-1_amd64_bullseye.deb
-sudo apt install -f
-
-git clone https://www.github.com/Airblader/i3 i3-gaps
-cd i3-gaps && mkdir -p build && cd build && meson ..
+# Clone and install i3-gaps
+cd ~
+git clone https://github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+meson build
+cd build
 ninja
 sudo ninja install
-cd ../..
+cd ~
 
-pip3 install pywal
+# Install pywal for theme management
+pip3 install --user pywal
 
-mkdir -p ~/.config/i3
-mkdir -p ~/.config/compton
-mkdir -p ~/.config/rofi
-mkdir -p ~/.config/alacritty
+# Create config directories and copy configuration files
+mkdir -p ~/.config/i3 ~/.config/compton ~/.config/rofi ~/.config/alacritty
 cp .config/i3/config ~/.config/i3/config
 cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
@@ -41,9 +46,14 @@ cp .config/compton/compton.conf ~/.config/compton/compton.conf
 cp .config/rofi/config ~/.config/rofi/config
 cp .fehbg ~/.fehbg
 cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
-cp -r .wallpaper ~/.wallpaper 
+cp -r .wallpaper ~/.wallpaper
 
-echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme. To have the wallpaper set on every boot edit ~.fehbg"
-echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
+# Apply wallpaper settings and set theme
+feh --bg-scale ~/.wallpaper/default.jpg
+lxappearance --set arc-dark
 
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Completion message
+echo "Installation complete! Reboot and select i3 from the login screen."
